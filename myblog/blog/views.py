9574fa654602ea564482c8c572b6b2bbbf3a3daf
egomaabura <k1222ars@gmail.com>
2024-06-03 13:58:59 +0900

    [add] delete feature

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -24,8 +24,20 @@ def create(request):
 
     articles = Article.objects.all()
     context = {
-        "message": "Welcome to My Blog",
-        "description": "This is my blog.",
+        "message": "Created Article",
+        "description": "記事を作成しました。",
+        "articles": articles
+    }
+    return render(request, "blog/index.html", context)
+
+def delete(request, id):
+    article = Article.objects.get(pk=id)
+    article.delete()
+
+    articles = Article.objects.all()
+    context = {
+        "message": f"Deleted Article(id = {id})",
+        "description": "記事を削除しました。",
         "articles": articles
     }
     return render(request, "blog/index.html", context)
